7635308bebbd9f45e978c75430a0bf5be5f870f8
goga <goga@GOGAL-LIN>
2024-01-31 13:12:06 +0300

    SkIDE unit categories tree added to main window

--- org.toxsoft.skf.bridge.cfg.opcua.skide/src/org/toxsoft/skf/bridge/cfg/opcua/skide/main/SkideUnitBridgeCfgOpcUa.java
+++ org.toxsoft.skf.bridge.cfg.opcua.skide/src/org/toxsoft/skf/bridge/cfg/opcua/skide/main/SkideUnitBridgeCfgOpcUa.java
@@ -5,6 +5,7 @@ import static org.toxsoft.core.tslib.av.metainfo.IAvMetaConstants.*;
 import static org.toxsoft.skf.bridge.cfg.opcua.skide.ISkidePluginBridgeCfgOpcUaConstants.*;
 import static org.toxsoft.skf.bridge.cfg.opcua.skide.ISkidePluginBridgeCfgOpcUaSharedResources.*;
 import static org.toxsoft.skide.core.ISkideCoreConstants.*;
+import static org.toxsoft.skide.core.api.ucateg.ISkideUnitCategoryConstants.*;
 
 import org.toxsoft.core.tsgui.bricks.ctx.*;
 import org.toxsoft.core.tslib.av.opset.impl.*;
@@ -28,6 +29,7 @@ public class SkideUnitBridgeCfgOpcUa
     super( UNIT_ID, OptionSetUtils.createOpSet( //
         TSID_NAME, STR_SKIDE_BRIDGE_CFG_OPC_UA_UNIT, //
         TSID_DESCRIPTION, STR_SKIDE_BRIDGE_CFG_OPC_UA_UNIT_D, //
+        OPDEF_SKIDE_UNIT_CATEGORY, UCATEGID_EXTERNAL_SYSTEMS, //
         TSID_ICON_ID, ICONID_CFG_SKIDE_PLUGIN //
     ), aContext, aCreator );
     unitActions().add( ACDEF_ABOUT );
