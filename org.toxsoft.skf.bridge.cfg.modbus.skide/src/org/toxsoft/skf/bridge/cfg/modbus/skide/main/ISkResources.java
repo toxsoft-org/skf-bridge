fcfd8f9baa4ffc4c351a8e08a202fb4848a39a01
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-05-17 09:35:58 +0300

    localization

---
+++ org.toxsoft.skf.bridge.cfg.modbus.skide/src/org/toxsoft/skf/bridge/cfg/modbus/skide/main/ISkResources.java
@@ -0,0 +1,15 @@
+package org.toxsoft.skf.bridge.cfg.modbus.skide.main;
+
+/**
+ * Localizable resources.
+ *
+ * @author dima
+ */
+public interface ISkResources {
+
+  /**
+   * {@link SkideUnitBridgeCfgModbusS5Mapping}
+   */
+  String STR_N_MODBUS_S5_CONFIG_EDITOR = "Редактор Modbus<->Uskat";
+  String STR_D_MODBUS_S5_CONFIG_EDITOR = "Редактор конфигурации Modbus<->Uskat";
+}
