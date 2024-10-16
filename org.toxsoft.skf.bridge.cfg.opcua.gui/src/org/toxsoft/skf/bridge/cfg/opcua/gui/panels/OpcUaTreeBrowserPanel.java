5369fc3ba7b9fc0ec962c0c6d1b085433eaf6be4
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-10-16 09:28:47 +0300

    small bug fixes

--- org.toxsoft.skf.bridge.cfg.opcua.gui/src/org/toxsoft/skf/bridge/cfg/opcua/gui/panels/OpcUaTreeBrowserPanel.java
+++ org.toxsoft.skf.bridge.cfg.opcua.gui/src/org/toxsoft/skf/bridge/cfg/opcua/gui/panels/OpcUaTreeBrowserPanel.java
@@ -1236,13 +1236,25 @@ public class OpcUaTreeBrowserPanel
       // находим родительский UaNode
       // UaTreeNode parentNode = aItemProvider.nodeById( obj.id() );
       Skid parentSkid = new Skid( aClassInfo.id(), obj.id() );
-      OpcUaUtils.getTreeSectionNameByConfig( OpcUaUtils.SECTID_OPC_UA_NODES_2_SKIDS_TEMPLATE, opcUaServerConnCfg );
+      // OpcUaUtils.getTreeSectionNameByConfig( OpcUaUtils.SECTID_OPC_UA_NODES_2_SKIDS_TEMPLATE, opcUaServerConnCfg );
 
       NodeId parentNodeId = OpcUaUtils.nodeBySkid( aContext, parentSkid, opcUaServerConnCfg );
-      TsIllegalStateRtException.checkNull( parentNodeId,
-          "Can't find nodeId for Skid: %s .\n Check section %s in file data-storage.ktor", parentSkid.toString(),
-          OpcUaUtils.getTreeSectionNameByConfig( OpcUaUtils.SECTID_OPC_UA_NODES_2_SKIDS_TEMPLATE,
-              opcUaServerConnCfg ) );
+      // тут отрабатываем ситуацию когда утеряна метаинформация
+      if( parentNodeId == null ) {
+        ETsDialogCode retCode = TsDialogUtils.askYesNoCancel( getShell(),
+            "Can't find nodeId for Skid: %s .\n Check section %s in file data-storage.ktor .\n Do you want to continue?",
+            parentSkid.toString(), OpcUaUtils
+                .getTreeSectionNameByConfig( OpcUaUtils.SECTID_OPC_UA_NODES_2_SKIDS_TEMPLATE, opcUaServerConnCfg ) );
+        if( retCode == ETsDialogCode.CANCEL || retCode == ETsDialogCode.CLOSE || retCode == ETsDialogCode.NO ) {
+          return;
+        }
+        continue;
+      }
+      // old version
+      // TsIllegalStateRtException.checkNull( parentNodeId,
+      // "Can't find nodeId for Skid: %s .\n Check section %s in file data-storage.ktor", parentSkid.toString(),
+      // OpcUaUtils.getTreeSectionNameByConfig( OpcUaUtils.SECTID_OPC_UA_NODES_2_SKIDS_TEMPLATE,
+      // opcUaServerConnCfg ) );
       UaTreeNode parentNode = findParentNode( treeNodes, parentNodeId );
       // привязываем команды
       for( IDtoCmdInfo cmdInfo : aClassInfo.cmds().list() ) {
