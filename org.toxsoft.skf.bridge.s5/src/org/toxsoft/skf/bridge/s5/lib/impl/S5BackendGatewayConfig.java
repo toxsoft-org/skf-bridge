687a9cff8eab27b20008a75d49825e00e63add92
mvk <kovach.mike@gmail.com>
2024-12-23 06:00:37 +0700

    s5 gateways: +default login, default password (configuration params)

--- org.toxsoft.skf.bridge.s5/src/org/toxsoft/skf/bridge/s5/lib/impl/S5BackendGatewayConfig.java
+++ org.toxsoft.skf.bridge.s5/src/org/toxsoft/skf/bridge/s5/lib/impl/S5BackendGatewayConfig.java
@@ -27,11 +27,40 @@ public final class S5BackendGatewayConfig
   public static final String SYBSYSTEM_ID_PREFIX = ISkHardConstants.SK_SYSEXT_SERVICE_ID_PREFIX + ".gateways";
 
   /**
+   * Логин пользователя для подключения к удаленному серверу по умолчанию.
+   */
+  public static final IDataDef DEFAULT_LOGIN = register( SYBSYSTEM_ID_PREFIX + ".login", STRING, //$NON-NLS-1$
+      TSID_NAME, N_LOGING, //
+      TSID_DESCRIPTION, D_LOGING, //
+      TSID_IS_NULL_ALLOWED, AV_FALSE, //
+      TSID_DEFAULT_VALUE, avStr( "root" ) );
+
+  /**
+   * Пароль пользователя для подключения к удаленному серверу по умолчанию.
+   */
+  public static final IDataDef DEFAULT_PASSWORD = register( SYBSYSTEM_ID_PREFIX + ".password", STRING, //$NON-NLS-1$
+      TSID_NAME, N_PASSWORD, //
+      TSID_DESCRIPTION, D_PASSWORD, //
+      TSID_IS_NULL_ALLOWED, AV_FALSE, //
+      TSID_DEFAULT_VALUE, avStr( "1" ) );
+
+  /**
+   * Интервал времени (в сутках) используемый для синхронизации данных между локальным и удаленным сервером при
+   * образовании связи.
+   * <p>
+   */
+  public static final IDataDef SYNC_INTERVAL = register( SYBSYSTEM_ID_PREFIX + ".syncInterval", INTEGER, //$NON-NLS-1$
+      TSID_NAME, N_SYNC_INTERVAL, //
+      TSID_DESCRIPTION, D_SYNC_INTERVAL, //
+      TSID_IS_NULL_ALLOWED, AV_FALSE, //
+      TSID_DEFAULT_VALUE, avInt( 3 ) );
+
+  /**
    * Список описаний шлюзов с которыми работает сервер
    * <p>
    * TODO: register SkGatewayConfigurationListKeeper.KEEPER
    */
-  public static final IDataDef GATEWAYS = register( ".configurations", VALOBJ, //$NON-NLS-1$
+  public static final IDataDef GATEWAYS = register( SYBSYSTEM_ID_PREFIX + ".configurations", VALOBJ, //$NON-NLS-1$
       TSID_NAME, N_GATEWAYS, //
       TSID_DESCRIPTION, D_GATEWAYS, //
       TSID_IS_NULL_ALLOWED, AV_FALSE, //
@@ -41,6 +70,9 @@ public final class S5BackendGatewayConfig
    * Все параметры подсистемы.
    */
   public static final IStridablesList<IDataDef> ALL_GATEWAYS_OPDEFS = new StridablesList<>( //
+      DEFAULT_LOGIN, //
+      DEFAULT_PASSWORD, //
+      SYNC_INTERVAL, //
       GATEWAYS //
   );
 }
