0fa74ea4ac51fa4d29440b59eca2e751ceea7245
mvk <kovach.mike@gmail.com>
2025-01-27 04:22:36 +0700

    S5Gateway: +workaround (do not query events) 4

--- org.toxsoft.skf.bridge.s5/src/org/toxsoft/skf/bridge/s5/supports/S5Gateway.java
+++ org.toxsoft.skf.bridge.s5/src/org/toxsoft/skf/bridge/s5/supports/S5Gateway.java
@@ -1143,7 +1143,7 @@ class S5Gateway
     TsNullArgumentRtException.checkNulls( aHqService, aEventIds, aInterval );
     // TODO: mvkd ошибка обращения к query service у которой еше не инициализировано sharedConnection
     if( true ) {
-      return (ITimedList<SkEvent>)ITimedList.EMPTY;
+      return new TimedList<SkEvent>();
     }
     // Параметры запроса
     IOptionSetEdit options = new OptionSet( OptionSetUtils.createOpSet( //
