47f2c8bd42dce6b477e463c3b79788a2b786a543
tsdev <tsdev@baiterek-arm-2>
2025-06-27 20:14:57 +0500

    save current state

--- org.toxsoft.skf.bridge.cfg.opcua.gui/src/org/toxsoft/skf/bridge/cfg/opcua/gui/utils/OpcUaUtils.java
+++ org.toxsoft.skf.bridge.cfg.opcua.gui/src/org/toxsoft/skf/bridge/cfg/opcua/gui/utils/OpcUaUtils.java
@@ -474,7 +474,13 @@ public class OpcUaUtils {
    */
   public static Class<?> getNodeDataTypeClass( UaVariableNode aEntity ) {
     // new verion of max
-    Class<?> retVal = TypeUtil.getBackingClass( aEntity.getDataType() );
+    Class<?> retVal = null;
+    try {
+      retVal = TypeUtil.getBackingClass( aEntity.getDataType() );
+    }
+    catch( Exception ex ) {
+      LoggerUtils.errorLogger().error( ex );
+    }
     // dima 29.02.24 на Siemens верхний метод срабатывает не всегда, а нижний работает
     if( retVal == null ) {
       // old version of dima
